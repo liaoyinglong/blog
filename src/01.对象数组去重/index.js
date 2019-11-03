901a66f8511e207922c5b9e4a3ffbd911c24e0e0
liaoyinglong <vigossliao@gmail.com>
2019-11-03 18:05:49 +0800

    :sparkles: 添加01

---
+++ src/01.对象数组去重/index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {};
+// id相同的保留后一个
+var responseList = [
+    { id: 1, a: 1 },
+    { id: 2, a: 2 },
+    { id: 2, a: 11 },
+    { id: 3, a: 3 },
+    { id: 3, a: 33 },
+    { id: 1, a: 4 }
+];
+var hasMap = new Map();
+var res = responseList.reduce(function (res, cur) {
+    var i = hasMap.get(cur.id);
+    if (i !== void 0) {
+        res[i] = cur;
+    }
+    else {
+        hasMap.set(cur.id, res.push(cur) - 1);
+    }
+    return res;
+}, []);
+console.log(res);

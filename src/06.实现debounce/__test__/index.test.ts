622f139acea71a0e51b1ba3e242cc6ca1f10e9e1
liaoyinglong <vigossliao@gmail.com>
2019-11-05 17:15:58 +0800

    :white_check_mark: Adding tests.

--- src/06.实现debounce/__test__/index.test.ts
+++ src/06.实现debounce/__test__/index.test.ts
@@ -1,36 +1,16 @@
 import { debounce } from "../index";
 
-jest.useRealTimers();
+jest.useFakeTimers();
 
 describe("测试 debounce", () => {
-  it("短时间调用多次，实际只能一次", done => {
-    let mockFn = jest.fn(() => {
-      expect(mockFn).toBeCalled();
-      expect(mockFn).toBeCalledTimes(1);
-      done();
-    });
-
+  it("短时间调用多次，实际只能一次", () => {
+    let mockFn = jest.fn();
     let debouncedFn = debounce(mockFn, 3000);
-
     for (let i = 0; i < 10; i++) {
       debouncedFn();
     }
-  });
-
-  it("延迟一段时间执行两次，实际只能一次", done => {
-    let mockFn = jest.fn(() => {
-      expect(mockFn).toBeCalled();
-      expect(mockFn).toBeCalledTimes(1);
-      done();
-    });
-
-    let debouncedFn = debounce(mockFn, 3000);
-
-    setTimeout(() => {
-      debouncedFn();
-    }, 100);
-    setTimeout(() => {
-      debouncedFn();
-    }, 200);
+    jest.runAllTimers();
+    expect(mockFn).toBeCalled();
+    expect(mockFn).toBeCalledTimes(1);
   });
 });

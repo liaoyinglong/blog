7744aa51c74c73c37d980ac82847a86647782e75
liaoyinglong <vigossliao@gmail.com>
2019-11-05 23:15:02 +0800

    :bug: Fixing a bug.

--- test/shared/sleep.test.ts
+++ test/shared/sleep.test.ts
@@ -6,7 +6,10 @@ describe("测试 sleep 方法", () => {
   it("睡眠1S后执行", () => {
     const fn = jest.fn();
 
-    const res = sleep(1000, "aa");
+    const res = sleep(1000, "aa").then(val => {
+      fn(val);
+      return val;
+    });
 
     jest.advanceTimersByTime(500);
     expect(fn).not.toBeCalled();
@@ -14,9 +17,10 @@ describe("测试 sleep 方法", () => {
     jest.advanceTimersByTime(500);
     expect(res).resolves.toBe("aa");
 
-    jest.runAllTimers();
-    res.then(fn).then(() => {
+    res.then(() => {
       expect(fn).toBeCalledTimes(1);
+      expect(fn).toBeCalledWith("aa");
     });
+    jest.runAllTimers();
   });
 });

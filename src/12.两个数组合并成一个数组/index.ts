6349a3f506f947d6261ac87a1267d0893a93bd11
liaoyinglong <vigossliao@gmail.com>
2019-11-14 18:28:23 +0800

    :sparkles: Introducing new features.

--- src/12.两个数组合并成一个数组/index.ts
+++ src/12.两个数组合并成一个数组/index.ts
@@ -9,19 +9,20 @@ const arr1 = ["A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2"];
 const arr2 = ["A", "B", "C", "D"];
 
 function merge(arr1: string[], arr2: string[]) {
+  let insetIndex = 0;
   for (let i = 0; i < arr2.length; i++) {
     let str = arr2[i];
 
-    for (let j = 0; j < arr1.length; j++) {
+    for (let j = insetIndex; j < arr1.length; j++) {
       const cur = arr1[j];
       const next = arr1[j + 1];
       if (cur.startsWith(str) && !next?.startsWith(str)) {
         insetByIndex(arr1, j + 1, str);
+        insetIndex = j + 1;
         break;
       }
     }
   }
-
   return arr1;
 }
 
